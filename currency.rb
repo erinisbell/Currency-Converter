d95c251152cc177776e096386f57b7ca0316ecf5
Erin Isbell <erinisbell@users.noreply.github.com>
2015-10-08 15:09:15 -0500

    Update currency.rb

--- currency.rb
+++ currency.rb
@@ -22,15 +22,16 @@ class Currency
     else 
       puts "Not the same Country Code!"
     end 
-end
+  end
 
 
-#def - obj1 
-#if self.currency
-
-#else 
- # puts "Not the same Country Code"
-#end 
+  def - obj
+    if self.currency_code = obj.currency_code
+      return Currency.new(currency_code, obj.amount - self.amount)
+    else 
+     puts "Not the same Country Code"
+   end
+ end 
 
 end 
 
@@ -39,3 +40,4 @@ puts currency1 = Currency.new(1, "USD")
 puts currency2 = Currency.new(2, "USD")
 puts currency1.currency_code == currency2.currency_code
 puts currency1 + currency2
+puts currency1 - currency2
