27121f13d56053c52256668557b67a5ce620e9de
Erin Isbell <erinisbell@users.noreply.github.com>
2015-10-08 14:53:05 -0500

    Update currency.rb

--- currency.rb
+++ currency.rb
@@ -1,31 +1,33 @@
-
 class Currency
-	attr_accessor :amount, :currency_code
+  attr_accessor :amount, :currency_code
 
 
-	def initialize amount, currency_code
-		@amount = amount 
-		@currency_code = currency_code
-	end
+  def initialize amount, currency_code
+    @amount = amount 
+    @currency_code = currency_code
+  end
 
 
-	def to_s
-		"#{@amount} : #{currency_code}"
-	end
+  def to_s
+    "#{@amount} : #{currency_code}"
+  end
 
- def <=> amount 
-  @amount <=> self.amount.id
-end 
+  def <=> currency 
+    self.currency_code == currency.currency_code
+  end 
+
+  #def + 
+
+  #end 
 
 
-#def + amount
- # if self.amount == amount
- #  added_amount = cur.current_amount + self.current_amount
-# end
+#def - 
+
+#end 
 
 end 
 
 
 puts currency1 = Currency.new(1, "USD")
-puts currency2 = Currency.new(2, "USD")
-puts currency1 != currency2
+puts currency2 = Currency.new(2, "CAD")
+puts currency1.currency_code == currency2.currency_code
