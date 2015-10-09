922efbb0a7a3118bf85e58afd495d58d80cb0ae5
Erin Isbell <erinisbell@users.noreply.github.com>
2015-10-09 15:03:12 -0500

    Update converter.rb

--- converter.rb
+++ converter.rb
@@ -1,38 +1,46 @@
 require_relative 'UnknownCurrencyCodeError'
 
 class Converter < UnknownCurrencyCodeError
-	attr_accessor :code, :rate, :hash
-  HASH = {
-      "USD" => {"$" => "1.0", "1.0" => "$"},
-      "EUR" => {"€" => ".88", ".88" => "€"},
-      "GBP" => {"£" => ".65", ".65" => "£"},
-  }
+	attr_accessor :code, :rate
 
-	def initialize code, rate
-		@code = code
-		@rate = rate 
-
+  
+  def initialize code, rate
+    @code = code
+    @rate = rate 
   end	
 
-  def exchange currency, result
-		if currency == "$" || currency == "€" || currency == "£" # can you put currency == @hash?
-     return {currency.convert(Currency.new(1, :USD), :USD) => Currency.new(1, :USD)} 
-     #currency.rate.to_i * @hash[currency.code][result]
-     puts "your new amount is #{result}"
-   elsif currency != "$" || currency != "€" || currency != "£" # again, is there way to index the hash?
-    raise UnknownCurrencyCodeError
-    end
-  end
+
+def to_s
+      "#{@code} : #{rate}"
+end 
+
+
+def <=> obj1
+  if self.code == code
+    true
+  else self.rate <=> rate
+  end 
+end
+
+
+def convert currency, target_id
+  rate = @code[currency.currency_code][target_id]
+  target_amount = rate * currency.amount
+  return "#{target_id}: #{target_amount}"
+end 
+
 end
 
 
 
-puts "What Currency Would You like to Convert?"
-user = gets.chomp
 
-puts "How much would you like to Exchange?"
-user = gets.chomp.to_i.exchange
- begin 
- rescue UnknownCurrencyCodeError
-  puts "I dont know that country code. Try Again."
-end 
+HASH = {
+  "USD" => {"USD" => 1, "EUR" => 2, "GBP" =>  4},
+  "EUR" => {"USD" => 0.5, "EUR" => 1, "GBP" => 2},
+  "GBP" => {"USD" => 0.25, "EUR" => 0.5, "GBP" => 1},
+
+}
+
+conversion_rates = Converter.new(HASH)
+erinsMulah = Converter.new(1.0, 'USD')
+puts conversion_rates.convert(erinsMulah, 'GBP')
