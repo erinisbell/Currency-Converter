27ff5af36bc64c1a3816bfef5d735eba012cac2a
Erin Isbell <erinisbell@users.noreply.github.com>
2015-10-11 13:58:24 -0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -3,17 +3,17 @@ require_relative 'converter'
 
 
 HASH = {
-  "USD" => {"USD" => 1, "EUR" => 2, "GBP" =>  4},
-  "EUR" => {"USD" => 0.5, "EUR" => 1, "GBP" => 2},
-  "GBP" => {"USD" => 0.25, "EUR" => 0.5, "GBP" => 1},
+  "USD" => {"USD" => 1, "EUR" => 0.88, "GBP" =>  0.65},
+  "EUR" => {"USD" => 1.14, "EUR" => 1, "GBP" => 0.74},
+  "GBP" => {"USD" => 1.53, "EUR" => 1.35, "GBP" => 1},
+  "$" => {"USD" => 1, "EUR" => 0.88, "GBP" =>  0.65},
+  "€" => {"USD" => 1.14, "EUR" => 1, "GBP" => 0.74},
+  "£" => {"USD" => 1.53, "EUR" => 1.35, "GBP" => 1},
 
 }
 
 
-begin 
-  rescue UnknownCurrencyCodeError
-  puts "I dont know that country code. Try Again."
-end 
+
 
 
 puts "What Currency Would You Like to Convert?"
