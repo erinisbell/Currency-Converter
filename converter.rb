fc0f21cc59bf9578b26dcc280646099966491230
Erin Isbell <erinisbell@users.noreply.github.com>
2015-10-11 15:12:01 -0500

    Update converter.rb

--- converter.rb
+++ converter.rb
@@ -4,15 +4,18 @@ require_relative 'UnknownCurrencyCodeError'
 class Converter < UnknownCurrencyCodeError
 	attr_accessor :code
 
-  
-  def initialize code
-    @code = code
-  end	
 
-def convert currency, target_id
-  rate = @code[currency.currency_code][target_id]
-  target_amount = rate * currency.amount
-  return "#{target_id}: #{target_amount}"
-end 
+	def initialize code
+		@code = code
+	end	
 
+	def convert currency, target_id
+		if rate = @code[currency.currency_code][target_id]
+			target_amount = rate * currency.amount
+			return "#{target_id}: #{target_amount}"
+			if rate != @code[currency.currency_code][target_id]
+				raise UnknownCurrencyCodeError 
+			end
+		end 
+	end
 end
